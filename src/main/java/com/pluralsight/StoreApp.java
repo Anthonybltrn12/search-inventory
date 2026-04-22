a5cfb3acb18438e89e70d82417b6a6ef7b8a38ed
antbeltran <anthonybltrn@gmail.com>
2026-04-22 14:43:16 -0500

    add csv to start bonus

--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -11,18 +11,19 @@ public class StoreApp {
         System.out.println("----Inventory List----");
         //looping through array
         for(int i = 0; i < inventoryList.size(); i++){
-            System.out.println(inventoryList.get(i).getName());
+            Product product = inventoryList.get(i);
+            System.out.printf("%s %s cost $%.2f \n", product.getId(), product.getName(), product.getPrice());
         }
     }
         //method to create values for the array
     public static ArrayList<Product> getInventory(){
         ArrayList<Product> inventory = new ArrayList<Product>();
 
-        inventory.add(new Product(1, "football" , 50.00));
-        inventory.add(new Product(2, "basketball" , 30.00));
-        inventory.add(new Product(3, "baseball" , 10.00));
-        inventory.add(new Product(4, "hockey puck" , 20.00));
-        inventory.add(new Product(5, "soccer ball" , 30.00));
+        inventory.add(new Product(1, "football" , 50));
+        inventory.add(new Product(2, "basketball" , 30));
+        inventory.add(new Product(3, "baseball" , 10));
+        inventory.add(new Product(4, "hockey puck" , 20));
+        inventory.add(new Product(5, "soccer ball" , 30));
 
         return inventory;
 
