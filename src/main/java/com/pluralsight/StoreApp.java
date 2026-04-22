390398e4d7408bde90705259197dd703a2705058
antbeltran <anthonybltrn@gmail.com>
2026-04-22 14:24:23 -0500

    created the first 5 products in the array

---
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class StoreApp {
+    public static void main(String[] args) {
+
+    }
+
+    public static ArrayList<Product> getInvenotry(){
+        ArrayList<Product> inventory = new ArrayList<Product>();
+
+        inventory.add(new Product(1, "football" , 50.00));
+        inventory.add(new Product(2, "basketball" , 30.00));
+        inventory.add(new Product(3, "baseball" , 10.00));
+        inventory.add(new Product(4, "hockey puck" , 20.00));
+        inventory.add(new Product(5, "soccer ball" , 30.00));
+
+        return inventory;
+    }
+}
