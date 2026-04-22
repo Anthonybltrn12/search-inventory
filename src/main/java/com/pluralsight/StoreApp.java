456d3974cf7224e5ee32a7670a0fa1b38a81be00
antbeltran <anthonybltrn@gmail.com>
2026-04-22 15:12:15 -0500

    got the files data to display, was using wrong csv file at first

--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -1,31 +1,44 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class StoreApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //creating inveotry arraylist
         ArrayList<Product> inventoryList = getInventory();
 
         System.out.println("----Inventory List----");
         //looping through array
         for(int i = 0; i < inventoryList.size(); i++){
+
             Product product = inventoryList.get(i);
             System.out.printf("%s %s cost $%.2f \n", product.getId(), product.getName(), product.getPrice());
         }
     }
+
         //method to create values for the array
-    public static ArrayList<Product> getInventory(){
-        ArrayList<Product> inventory = new ArrayList<Product>();
+    public static ArrayList<Product> getInventory() throws IOException {
+        try {
+            //reader for csv file
+            ArrayList<Product> inventory = new ArrayList<Product>();
+            FileReader fileReader = new FileReader("src/main/resources/inventory.csv");
+            BufferedReader buffReader = new BufferedReader(fileReader);
 
-        inventory.add(new Product(1, "football" , 50));
-        inventory.add(new Product(2, "basketball" , 30));
-        inventory.add(new Product(3, "baseball" , 10));
-        inventory.add(new Product(4, "hockey puck" , 20));
-        inventory.add(new Product(5, "soccer ball" , 30));
+            String productLine;
+            while ((productLine = buffReader.readLine()) != null) {
+                String[] productArray = productLine.split("\\|");
+                inventory.add(new Product(Integer.parseInt(productArray[0]), productArray[1], Double.parseDouble(productArray[2])));
+            }
 
-        return inventory;
+            return inventory;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
